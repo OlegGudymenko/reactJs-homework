69ef19d70058e314ee8c3298efbc4e96d99f52b0
oleg <oleggudymenko96@gmail.com>
2017-10-13 18:27:13 +0300

    lesson1

--- src/App.js
+++ src/App.js
@@ -2,8 +2,49 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+
+class Timer2  extends Component {
+  constructor(props){
+    super(props)
+
+    this.state = {
+      date : new Date(),
+    }
+  }
+
+  componentDidMount(){
+    setInterval( () => {
+      this.setState({
+        date : new Date()
+      })
+    } ,1000);
+  }
+
+  render(){
+    const { showFullDate } = this.props
+    
+    if( showFullDate ){
+      return(
+
+        <h2> {this.state.date.toLocaleTimeString()}</h2>
+      )
+    }else{
+      return null;
+    }
+  }
+}
+
+
 class App extends Component {
+  constructor(props){
+    super(props)
+    this.state = {
+      // date : new Date(),
+    }
+  }
+
   render() {
+
     return (
       <div className="App">
         <header className="App-header">
@@ -13,6 +54,7 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+        <Timer2 showFullDate />
       </div>
     );
   }
